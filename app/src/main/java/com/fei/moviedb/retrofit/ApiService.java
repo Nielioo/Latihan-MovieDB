e9d9a6f702f89c5f8756ace94837b87b01e88c81
Daniel Aprillio <danielaprillio02@gmail.com>
2021-10-04 09:09:54 +0700

    4 oct 2021

--- app/src/main/java/com/fei/moviedb/retrofit/ApiService.java
+++ app/src/main/java/com/fei/moviedb/retrofit/ApiService.java
@@ -1,5 +1,21 @@
 package com.fei.moviedb.retrofit;
 
+import com.fei.moviedb.helper.Const;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiService {
+
+    private static Retrofit retrofit;
+    
+    public static ApiEndPoint endPoint(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Const.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        
+        return retrofit.create(ApiEndPoint.class);
+    }
 
 }
