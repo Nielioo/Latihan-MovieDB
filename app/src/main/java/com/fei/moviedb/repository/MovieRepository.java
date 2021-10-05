eebf1b611c3a06bb801b0779651285683ab26d44
Daniel Aprillio <danielaprillio02@gmail.com>
2021-10-05 10:44:46 +0700

    5 oct 2021  - create repository  - create viewmodel

---
+++ app/src/main/java/com/fei/moviedb/repository/MovieRepository.java
@@ -0,0 +1,44 @@
+package com.fei.moviedb.repository;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.fei.moviedb.helper.Const;
+import com.fei.moviedb.model.Movies;
+import com.fei.moviedb.retrofit.ApiService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieRepository {
+
+    private static MovieRepository repository;
+
+    private MovieRepository(){}
+
+    public static MovieRepository getInstance(){
+        if(repository==null){
+            repository = new MovieRepository();
+        }
+        return repository;
+    }
+
+    public MutableLiveData<Movies> getMovieData(String movieId){
+        final MutableLiveData<Movies> result = new MutableLiveData<>();
+
+        ApiService.endPoint().getMovieById(movieId, Const.API_KEY).enqueue(new Callback<Movies>() {
+            @Override
+            public void onResponse(Call<Movies> call, Response<Movies> response) {
+                result.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Movies> call, Throwable t) {
+
+            }
+        });
+
+        return result;
+    }
+
+}
---
+++ app/src/main/java/com/fei/moviedb/viewmodel/MovieViewModel.java
@@ -0,0 +1,34 @@
+package com.fei.moviedb.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.fei.moviedb.model.Movies;
+import com.fei.moviedb.repository.MovieRepository;
+
+public class MovieViewModel extends AndroidViewModel {
+
+    private MovieRepository repository;
+
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+        repository = MovieRepository.getInstance();
+    }
+
+    //==Begin of viewModel get movie by id
+
+    private MutableLiveData<Movies> resultGetMovieById = new MutableLiveData<>();
+    public void setResultGetMovieById(String movieId){
+        resultGetMovieById =  repository.getMovieData(movieId);
+    }
+    public LiveData<Movies> getResultGetMovieById(){
+        return resultGetMovieById;
+    }
+
+    //==End of viewModel get movie by id
+
+}
