f717ea05a7f76c0486142a99aa0cdcbed3e4e56b
Daniel Aprillio <danielaprillio02@gmail.com>
2021-10-25 08:22:28 +0700

    upcoming

---
+++ app/src/main/java/com/fei/moviedb/view/fragments/upcomingFragment.java
@@ -0,0 +1,66 @@
+package com.fei.moviedb.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fei.moviedb.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link upcomingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class upcomingFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public upcomingFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment upcomingFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static upcomingFragment newInstance(String param1, String param2) {
+        upcomingFragment fragment = new upcomingFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+    }
+}
