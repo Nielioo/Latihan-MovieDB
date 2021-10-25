c8b7f7b2832fa24acc959cde1775996636e5e928
Daniel Aprillio <danielaprillio02@gmail.com>
2021-10-25 08:22:46 +0700

    menu

---
+++ app/src/main/java/com/fei/moviedb/view/MainMenuActivity.java
@@ -0,0 +1,44 @@
+package com.fei.moviedb.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.fei.moviedb.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    BottomNavigationView mainMenu_bottomNavigation;
+    NavHostFragment mainMenu_navFragment;
+    Toolbar mainMenu_toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        initialize();
+
+    }
+
+    private void initialize() {
+        mainMenu_bottomNavigation = findViewById(R.id.mainMenu_bottomNavigation);
+        mainMenu_navFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.mainMenu_navFragment);
+
+        mainMenu_toolbar = findViewById(R.id.mainMenu_toolbar);
+        setSupportActionBar(mainMenu_toolbar);
+
+        //Dipakai kalau pakai action bar
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.nowPlayingFragment, R.id.upcomingFragment).build();
+        NavigationUI.setupActionBarWithNavController(MainMenuActivity.this, mainMenu_navFragment.getNavController(), appBarConfiguration);
+
+        NavigationUI.setupWithNavController(mainMenu_bottomNavigation, mainMenu_navFragment.getNavController());
+
+
+    }
+}
